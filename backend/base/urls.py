1880570492e53da742a8860f41dc140e886ab81a
许向 <x2x4com@gmail.com>
2019-04-30 10:19:47 +0800

    update

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('site/admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('/app/control', include(frontend_urls)),
+    path('app/control', include(frontend_urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
